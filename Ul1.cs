87bf8f9ccaf85fe8ac856d3ae0358ac66d931020
opilane <opilane@HP-CZC6297BB1>
2022-02-23 12:49:08 +0200

    Add project files.

---
+++ Ul1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Iseseiv
+{
+	class Ul1
+	{
+        // Varinat 7
+        // Ü1
+
+        /*static void ReplaceArr(int[,] arr)
+        {
+            for (int i = 1; i < arr.GetLength(0); i += 2)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    int temp = arr[i, j];
+                    arr[i, j] = arr[i - 1, j];
+                    arr[i - 1, j] = temp;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[,]
+            {
+                { 1, 2, 3 },
+                { 2, 3, 4 },
+                { 5, 6, 7 },
+                { 7, 9, 5 },
+                //{ 3, 8, 1 },
+                //{ 4, 7, 2 },
+                //{ 5, 4, 9 },
+                //{ 1, 1, 8 }
+            };
+            ReplaceArr(arr);
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }*/
+    }
+}
---
+++ Ul2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iseseiv
+{
+	public class Ul2
+	{
+		static void Main(string[] args)
+		{
+			string text = "On tekst. Otsi seda sõna kas siin on selle sõna või ei. ";
+			foreach (string i in text.Split(',', '.', ':', '!', '?', ' '))
+			{
+
+			}
+		}
+	}
+}
