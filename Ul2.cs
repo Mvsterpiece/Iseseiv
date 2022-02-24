3e53a19d239e4b84988ad0524e391c5b7d11415c
Денис Сайко <deonisii.sd@gmail.com>
2022-02-24 15:35:32 +0200

    asd

--- Ul2.cs
+++ Ul2.cs
@@ -6,13 +6,30 @@ namespace Iseseiv
 {
 	public class Ul2
 	{
-		static void Main(string[] args)
-		{
-			string text = "On tekst. Otsi seda sõna kas siin on selle sõna või ei. ";
-			foreach (string i in text.Split(',', '.', ':', '!', '?', ' '))
-			{
+        //Определите, присутствует ли в тексте, заданном в виде строки, некоторое слово (различие регистра игнорируйте).
+        /*static void Main(string[] args)
+        {
+            string text = "On tekst. Otsi seda sõna kas siin on selle sõna või ei. ";
+            string word = "tekst";
+            string[] teksts = text.Split(' ');
+            bool k = false;
 
-			}
-		}
-	}
+            foreach (string s in teksts)
+            {
+                if (word == text)
+                {
+                    k = true;
+                    break;
+                }
+                if (k)
+                {
+                    Console.WriteLine("Sona on tekstis");
+                }
+                else
+                {
+                    Console.WriteLine("Sona puudub selles teksis.");
+                }
+            }
+        }*/
+    }
 }
---
+++ Ul3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iseseiv
+{
+    internal class Ul3
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+        }
+    }
+}
